21387da499a0b0e42ba575a65dddc2acc37043cb
Priyanka7797 <133853878+Priyanka7797@users.noreply.github.com>
2023-07-19 17:04:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,14 @@
 
 if [[ $GIT_BRANCH == origin/dev ]]
 then
-        cd react
-        chmod +x build.sh
-        ./build.sh
+
         docker login --username=$docker_username --password=$docker_password
         docker tag react:v1 priyanka7797/dev:v1
         docker image push priyanka7797/dev:v1
 
 elif [[ $GIT_BRANCH == origin/main ]]
 then
-        cd react
-        chmod +x build.sh
-        ./build.sh
+
         docker login --username=$docker_username --password=$docker_password
         docker tag react:v1 priyanka7797/prod:v1
         docker image push priyanka7797/prod:v1
