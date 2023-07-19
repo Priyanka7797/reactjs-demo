19ee62b0e02a1931466aef55cc9296a71a42cdca
Priyanka7797 <133853878+Priyanka7797@users.noreply.github.com>
2023-07-19 16:36:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Set the Docker image name and tag
-IMAGE_NAME="your-image-name"
-TAG="latest"
-
-# Build the Docker image
-docker build -t $IMAGE_NAME:$TAG .
+docker build -t react:v1 .
 
 
 
