082c174294b7f6353bdbff3ff6772b08ca964bd6
Priyanka7797 <133853878+Priyanka7797@users.noreply.github.com>
2023-07-19 16:35:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,15 @@ then
         docker login --username=$docker_username --password=$docker_password
         docker tag react:v1 priyanka7797/dev:v1
         docker image push priyanka7797/dev:v1
+
+elif [[ $GIT_BRANCH == origin/main ]]
+then
+        cd react
+        chmod +x build.sh
+        ./build.sh
+        docker login --username=$docker_username --password=$docker_password
+        docker tag react:v1 priyanka7797/prod:v1
+        docker image push priyanka7797/prod:v1
+
         
 
